2406d006e912c09287a9ee03d35620f36b795674
hikosan2683 <68491145+hikosan2683@users.noreply.github.com>
2026-08-28 12:41:24 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -16,6 +16,8 @@ namespace 話すロボット {
     // ★★★文字を送る ★★★
     //% block="%text と話す"
     export function speakText (text: string) {
+                // ★文字を送る前に自動で初期化！
+        serial.redirect(SerialPin.P0, SerialPin.P1, BaudRate.BaudRate9600)
         serial.writeLine(text) // 入力された文字をそのまま相手に送る
     }
 
